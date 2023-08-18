12b32399ecccbe9253b6196d397ce6d4524b6552
asfafasa <giftofgodashish@gmail.com>
2023-08-19 01:00:50 +0530

    added sonme files

---
+++ client/src/components/Confirmation/Confirmation.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {
+  title: string;
+  details?: string;
+  actionButton: () => void;
+  closeUpButton: () => void;
+};
+
+const Confirmation = (props: Props) => {
+  return <div></div>;
+};
+
+export default Confirmation;
